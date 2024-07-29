b85befdf644496157bbf25fe01c73162748d50ed
Denis Savanin <superrr.den@list.ru>
2024-07-29 20:33:49 +0300

    Some fixes

--- src/index.js
+++ src/index.js
@@ -1,21 +1,92 @@
 class Character {
-constructor(name, type, health, level, attack, defence) {
-    this.name;
-    this.type;
-    this.health = 100;
-    this.level = 1;
-    this.attack;
-    this.defence;
-}
-}
+    constructor(name, type) {
+        const validTypes = ['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'];
 
-function makeCharacter(name, type, attack, defence) {
-    let character = 0;
-    if ((typeof name != String) && (name.length < 2) && (name.length > 10)) {
-        throw new Error('Нужна строка, мин - 2 символа, макс - 10 символов')
-    } else if ((type !== 'Bowman' ) && (type !== 'Swordsman' ) && (type !== 'Magician' ) && (type !== 'Undead' ) && (type !== 'Zombie' ) ) {
-        throw new Error('type - один из типов (строка): Bowman, Swordsman, Magician, Daemon, Undead, Zombie')
+        if (typeof name !== 'string' || name.length < 2 || name.length > 10) {
+            throw new Error('Имя должно быть мин. 2 символа, макс. 10 символов, тип Строка');
+        }
+
+        if (!validTypes.includes(type)) {
+            throw new Error(`Типы должны быть такими: ${validTypes.join(', ')}.`);
+        }
+
+        this.name = name;
+        this.type = type;
+        this.health = 100;
+        this.level = 1;
+        this.attack = 0;
+        this.defence = 0;
     }
+}
 
-    character = new Character(name, type, attack, defence)
+class Bowerman extends Character {
+    constructor(name) {
+        super(name, 'Bowman');
+        this.attack = 25;
+        this.defence = 25;
+    }
+}
+
+class Swordsman extends Character {
+    constructor(name) {
+        super(name, 'Swordsman');
+        this.attack = 40;
+        this.defence = 10;
+    }
+}
+
+class Magician extends Character {
+    constructor(name) {
+        super(name, 'Magician');
+        this.attack = 10;
+        this.defence = 40;
+    }
+}
+
+class Daemon extends Character {
+    constructor(name) {
+        super(name, 'Daemon');
+        this.attack = 10;
+        this.defence = 40;
+    }
+}
+
+class Undead extends Character {
+    constructor(name) {
+        super(name, 'Undead');
+        this.attack = 25;
+        this.defence = 25;
+    }
+}
+
+class Zombie extends Character {
+    constructor(name) {
+        super(name, 'Zombie');
+        this.attack = 40;
+        this.defence = 10;
+    }
+}
+
+try {
+    const bowman = new Bowerman('Legolas');
+    console.log(bowman);
+
+    const swordsman = new Swordsman('Aragorn');
+    console.log(swordsman);
+
+    const magician = new Magician('Gandalf');
+    console.log(magician);
+
+    const daemon = new Daemon('Azazel');
+    console.log(daemon);
+
+    const undead = new Undead('Voldemort');
+    console.log(undead);
+
+    const zombie = new Zombie('ZombieMan');
+    console.log(zombie);
+
+    const invalidCharacter = new Bowerman('L');
+} catch (error) {
+    console.error(error.message);
 }
