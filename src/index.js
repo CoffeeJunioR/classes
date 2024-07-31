8c19018967709cde2c41d46517b6e051144f36bf
Denis Savanin <superrr.den@list.ru>
2024-07-31 17:40:28 +0300

    Added some tests and funcs

--- src/index.js
+++ src/index.js
@@ -1,71 +1,9 @@
-class Character {
-    constructor(name, type) {
-        const validTypes = ['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'];
-
-        if (typeof name !== 'string' || name.length < 2 || name.length > 10) {
-            throw new Error('Имя должно быть мин. 2 символа, макс. 10 символов, тип Строка');
-        }
-
-        if (!validTypes.includes(type)) {
-            throw new Error(`Типы должны быть такими: ${validTypes.join(', ')}.`);
-        }
-
-        this.name = name;
-        this.type = type;
-        this.health = 100;
-        this.level = 1;
-        this.attack = 0;
-        this.defence = 0;
-    }
-}
-
-class Bowerman extends Character {
-    constructor(name) {
-        super(name, 'Bowman');
-        this.attack = 25;
-        this.defence = 25;
-    }
-}
-
-class Swordsman extends Character {
-    constructor(name) {
-        super(name, 'Swordsman');
-        this.attack = 40;
-        this.defence = 10;
-    }
-}
-
-class Magician extends Character {
-    constructor(name) {
-        super(name, 'Magician');
-        this.attack = 10;
-        this.defence = 40;
-    }
-}
-
-class Daemon extends Character {
-    constructor(name) {
-        super(name, 'Daemon');
-        this.attack = 10;
-        this.defence = 40;
-    }
-}
-
-class Undead extends Character {
-    constructor(name) {
-        super(name, 'Undead');
-        this.attack = 25;
-        this.defence = 25;
-    }
-}
-
-class Zombie extends Character {
-    constructor(name) {
-        super(name, 'Zombie');
-        this.attack = 40;
-        this.defence = 10;
-    }
-}
+import Bowerman from "./heroes/Bowerman.js";
+import Daemon from "./heroes/daemon.js";
+import Magician from "./heroes/magician.js";
+import Swordsman from "./heroes/swordsman.js";
+import Undead from "./heroes/undead.js";
+import Zombie from "./heroes/zombie.js";
 
 try {
     const bowman = new Bowerman('Legolas');
